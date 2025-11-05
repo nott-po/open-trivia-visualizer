f20d14b721163471430971d0f1bef05d8770e31f
nott-po <nesterovapolina55@gmail.com>
2025-11-05 17:32:22 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+    const [questions, setQuestions] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://opentdb.com/api.php?amount=50').then(response => response.json()).then(data => {
+            console.log('Fetched data:', data.results);
+            setQuestions(data.results);
+            setLoading(false);
+        }).catch(error => {
+            console.error('Error fetching data:', error);
+            setLoading(false);
+        })
+    }, []);
+
+    if(loading){
+        return(
+            <div className="App">
+                <h1>Trivia Questions Visualizer</h1>
+                <p>Loading questions...</p>
+            </div>
+        )
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Trivia Questions Visualizer</h1>
+        <p>Loaded  {questions.length} questions</p>
     </div>
   );
 }
